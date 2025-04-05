7897202fe06755b786f25a2cdce0f99b0aa3f78d
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-04-04 18:37:50 -0700

    Added Fairy Sapling/Stone Texture, Also added stone ore feature

--- src/main/java/com/bladecoldsteel/invigorateddimensions/world/gen/features/FairyFeatures.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/world/gen/features/FairyFeatures.java
@@ -8,10 +8,13 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.template.RuleTest;
+import net.minecraft.world.gen.feature.template.TagMatchRuleTest;
 import net.minecraft.world.gen.foliageplacer.BushFoliagePlacer;
 import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.gen.trunkplacer.FancyTrunkPlacer;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -19,7 +22,7 @@ public class FairyFeatures {
     public static final DeferredRegister<Feature<?>> FAIRY_FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, InvigoratedDimensions.MOD_ID);
 
     public static final class ConfiguredFeatures {
-
+        static final RuleTest OVERWORLD_FILLER = new TagMatchRuleTest(Tags.Blocks.STONE);
 
         public static final ConfiguredFeature<BaseTreeFeatureConfig, ?> FEYAN_TREE = Feature.TREE.configured(
                 new BaseTreeFeatureConfig.Builder(
@@ -29,6 +32,10 @@ public class FairyFeatures {
                         new FancyTrunkPlacer(6, 6, 8),
                         new TwoLayerFeature(2, 2, 3))
                         .ignoreVines().build());
+
+        public static final ConfiguredFeature<?, ?> FEYAN_STONE_PATCH = Feature.ORE.configured(
+                new OreFeatureConfig(OVERWORLD_FILLER, FeywildExpanseBlocks.FEYAN_STONE.get().defaultBlockState(), 33)
+        );
     }
 
     public static void registerConfiguredFeatures() {
@@ -37,6 +44,8 @@ public class FairyFeatures {
         register("feyan_tree", FairyFeatures.ConfiguredFeatures.FEYAN_TREE
                 .decorated(Placement.COUNT_EXTRA.configured(new AtSurfaceWithExtraConfig(1, 0.05f, 2)))
                 .decorated(Features.Placements.HEIGHTMAP_SQUARE));
+        //Ores
+        register("feyan_stone_patch", FairyFeatures.ConfiguredFeatures.FEYAN_STONE_PATCH.range(256).squared().count(15));
     }
 
     private static <FC extends IFeatureConfig> void register(String name, ConfiguredFeature<FC, ?> feature) {
