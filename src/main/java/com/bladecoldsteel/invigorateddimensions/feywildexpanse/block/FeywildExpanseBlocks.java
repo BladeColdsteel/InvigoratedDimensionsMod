2d12c3ceaa3f5b0b37f867db30f3581e01215272
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-04-04 18:37:39 -0700

    Added Fairy Sapling/Stone Texture, Also added stone ore feature

--- src/main/java/com/bladecoldsteel/invigorateddimensions/feywildexpanse/block/FeywildExpanseBlocks.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/feywildexpanse/block/FeywildExpanseBlocks.java
@@ -25,6 +25,13 @@ public class FeywildExpanseBlocks {
             () -> new RotatedPillarBlock(AbstractBlock.Properties.copy(Blocks.JUNGLE_LOG)
                     .harvestTool(ToolType.AXE)));
 
+    public static final RegistryObject<Block> FEYAN_STONE = registerBlock("feyan_stone",
+            () -> new Block(AbstractBlock.Properties.copy(Blocks.STONE)
+                    .harvestLevel(1)
+                    .harvestTool(ToolType.PICKAXE)
+                    .requiresCorrectToolForDrops()
+                    .strength(3f)));
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
