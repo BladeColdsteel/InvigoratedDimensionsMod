2c5ec9be76d594287e827f85c1ba8b939cc77bfb
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-04-06 16:01:26 -0700

    Added Grassy Cobblestone and Loot tables for Grassy blocks

--- src/main/java/com/bladecoldsteel/invigorateddimensions/grassyknoll/block/GrassyKnollBlocks.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/grassyknoll/block/GrassyKnollBlocks.java
@@ -32,6 +32,12 @@ public class GrassyKnollBlocks {
                     .harvestTool(ToolType.PICKAXE)
                     .requiresCorrectToolForDrops()
                     .strength(3f)));
+    public static final RegistryObject<Block> GRASSY_COBBLESTONE = registerBlock("grassy_cobblestone",
+            () -> new Block(AbstractBlock.Properties.copy(Blocks.COBBLESTONE)
+                    .harvestLevel(1)
+                    .requiresCorrectToolForDrops()
+                    .harvestTool(ToolType.PICKAXE)
+                    .strength(2f)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
