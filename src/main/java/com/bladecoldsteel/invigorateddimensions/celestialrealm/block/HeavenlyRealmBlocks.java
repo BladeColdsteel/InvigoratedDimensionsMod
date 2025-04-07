c29bacae30b98a85499c9ada764450f97a4fb261
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-04-06 20:48:27 -0700

    Added Floating Cobblestone and Loot tables for Floating blocks

--- src/main/java/com/bladecoldsteel/invigorateddimensions/celestialrealm/block/HeavenlyRealmBlocks.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/celestialrealm/block/HeavenlyRealmBlocks.java
@@ -32,6 +32,12 @@ public class HeavenlyRealmBlocks {
                     .harvestTool(ToolType.PICKAXE)
                     .requiresCorrectToolForDrops()
                     .strength(3f)));
+    public static final RegistryObject<Block> FLOATING_COBBLESTONE = registerBlock("floating_cobblestone",
+            () -> new Block(AbstractBlock.Properties.copy(Blocks.COBBLESTONE)
+                    .harvestLevel(1)
+                    .requiresCorrectToolForDrops()
+                    .harvestTool(ToolType.PICKAXE)
+                    .strength(2f)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
