8ef25879ca536c960a8f55309a0cf579c28eb2c4
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-04-07 06:36:44 -0700

    Added Corrosive Cobblestone and Loot tables for Corrosive blocks

--- src/main/java/com/bladecoldsteel/invigorateddimensions/corrosivefields/block/CorrosiveFieldsBlocks.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/corrosivefields/block/CorrosiveFieldsBlocks.java
@@ -31,6 +31,12 @@ public class CorrosiveFieldsBlocks {
                     .harvestTool(ToolType.PICKAXE)
                     .requiresCorrectToolForDrops()
                     .strength(3f)));
+    public static final RegistryObject<Block> CORROSIVE_COBBLESTONE = registerBlock("corrosive_cobblestone",
+            () -> new Block(AbstractBlock.Properties.copy(Blocks.COBBLESTONE)
+                    .harvestLevel(1)
+                    .requiresCorrectToolForDrops()
+                    .harvestTool(ToolType.PICKAXE)
+                    .strength(2f)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
