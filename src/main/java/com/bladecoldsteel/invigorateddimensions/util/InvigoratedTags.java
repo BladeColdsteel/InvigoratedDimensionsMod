70ddf34a94b59e3af5b9d09739bba605f00cf338
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-06-29 17:12:27 -0700

    Created Base Stone Tags

--- src/main/java/com/bladecoldsteel/invigorateddimensions/util/InvigoratedTags.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/util/InvigoratedTags.java
@@ -14,6 +14,34 @@ public class InvigoratedTags {
 
         public static final Tags.IOptionalNamedTag<Block> BASE_STONE_ELECTRIC_HIGHLANDS =
                 createTag("base_stone_electric_highlands");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_CAVERNOUS_COVE =
+                createTag("base_stone_cavernous_cove");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_CELESTIAL_REALM =
+                createTag("base_stone_celestial_realm");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_CORROSIVE_FIELDS =
+                createTag("base_stone_corrosive_fields");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_DEEP_TUNDRA =
+                createTag("base_stone_deep_tundra");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_DREAMLAND =
+                createTag("base_stone_dreamland");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_FEYWILD_EXPANSE =
+                createTag("base_stone_feywild_expanse");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_GHASTLY_MARSH =
+                createTag("base_stone_ghastly_marsh");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_GRASSY_KNOLL =
+                createTag("base_stone_grassy_knoll");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_INFINITE_DUNGEON =
+                createTag("base_stone_infinite_dungeon");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_INSECTOID_PARADISIO =
+                createTag("base_stone_insectoid_paradisio");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_METALLIC_MOUNTAINS =
+                createTag("base_stone_metallic_mountains");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_TERRA_NATA =
+                createTag("base_stone_terra_nata");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_VALLEY_OF_DEATH =
+                createTag("base_stone_valley_of_death");
+        public static final Tags.IOptionalNamedTag<Block> BASE_STONE_WATERY_DEPTHS =
+                createTag("base_stone_watery_depths");
         public static final Tags.IOptionalNamedTag<Block> ELEMENTAL_SAPLINGS_BLOCK =
                 createTag("elemental_saplings");
         public static final Tags.IOptionalNamedTag<Block> ELEMENTAL_STONES_BLOCK =
