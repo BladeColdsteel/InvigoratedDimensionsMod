8cff82587fc6668c8e45d39d74e488ee3bf925a4
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-07-25 21:36:46 -0700

    Fixed Draconic Rift Blocks Item reference

--- src/main/java/com/bladecoldsteel/invigorateddimensions/draconicrift/block/DraconicRiftBlocks.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/draconicrift/block/DraconicRiftBlocks.java
@@ -1,8 +1,8 @@
 package com.bladecoldsteel.invigorateddimensions.draconicrift.block;
 
 import com.bladecoldsteel.invigorateddimensions.InvigoratedDimensions;
-import com.bladecoldsteel.invigorateddimensions.deeptundra.item.DeepTundraItems;
 import com.bladecoldsteel.invigorateddimensions.draconicrift.item.DraconicRiftItemGroup;
+import com.bladecoldsteel.invigorateddimensions.draconicrift.item.DraconicRiftItems;
 import com.bladecoldsteel.invigorateddimensions.util.BlockHelper;
 import com.bladecoldsteel.invigorateddimensions.world.gen.features.trees.IceTree;
 import net.minecraft.block.*;
@@ -23,70 +23,70 @@ public class DraconicRiftBlocks {
 
     //Wooden Blocks
     public static final RegistryObject<Block> PLANKS = BlockHelper.register(
-            "draconic_planks", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_planks", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.woodenBlock(1.0F, 1, false),
             TAB
     );
     //Slabs
     public static final RegistryObject<SlabBlock> WOODEN_SLAB = BlockHelper.register(
-            "draconic_wooden_slab", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_wooden_slab", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.slabBlock(Material.WOOD, 2.0F,  ToolType.AXE, 1, SoundType.WOOD, false),
             TAB
     );
     //Stairs
     public static final RegistryObject<StairsBlock> WOODEN_STAIRS = BlockHelper.register(
-            "draconic_wooden_stairs", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_wooden_stairs", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.stairsBlock(PLANKS, Material.WOOD, 2.0F,  ToolType.AXE, 1, SoundType.WOOD, false),
             TAB
     );
     //Logs
     public static final RegistryObject<RotatedPillarBlock> LOG = BlockHelper.register(
-            "draconic_logs", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_logs", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.logBlock(2.0F),
             TAB
     );
     public static final RegistryObject<RotatedPillarBlock> WOOD = BlockHelper.register(
-            "draconic_wood", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_wood", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.logBlock(2.0F),
             TAB
     );
     //Fences
     public static final RegistryObject<FenceBlock> FENCE = BlockHelper.register(
-            "draconic_fence", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_fence", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.fenceBlock(1.0F),
             TAB
     );
     //Fence Gates
     public static final RegistryObject<FenceGateBlock> FENCE_GATE = BlockHelper.register(
-            "draconic_fence_gate", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_fence_gate", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.fenceGateBlock(1.0F),
             TAB
     );
     //Buttons
     public static final RegistryObject<WoodButtonBlock> WOODEN_BUTTON = BlockHelper.register(
-            "draconic_wooden_button", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_wooden_button", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.woodButton(1.0F),
             TAB
     );
     public static final RegistryObject<StoneButtonBlock> STONE_BUTTON = BlockHelper.register(
-            "draconic_stone_button", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_stone_button", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.stoneButton(2.0F),
             TAB
     );
     //Pressure Plates
     public static final RegistryObject<PressurePlateBlock> WOODEN_PRESSURE_PLATE = BlockHelper.register(
-            "draconic_wooden_pressure_plate", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_wooden_pressure_plate", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.woodPressurePlate(1.0F),
             TAB
     );
     public static final RegistryObject<PressurePlateBlock> STONE_PRESSURE_PLATE = BlockHelper.register(
-            "draconic_stone_pressure_plate", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_stone_pressure_plate", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.stonePressurePlate(2.0F),
             TAB
     );
     //Sapling
     public static final RegistryObject<SaplingBlock> SAPLING = BlockHelper.register(
-            "draconic_sapling", BLOCKS, DeepTundraItems.ITEMS,
+            "draconic_sapling", BLOCKS, DraconicRiftItems.ITEMS,
             BlockHelper.sapling(new IceTree(), Blocks.OAK_SAPLING),
             TAB);
 
