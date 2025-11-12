50ad17e65a533289f53ab50fa8e2f52334adbdfe
BladeColdsteel <152412820+BladeColdsteel@users.noreply.github.com>
2025-11-11 23:45:36 -0800

    Added Linear & Progressive Boss stats

--- src/main/java/com/bladecoldsteel/invigorateddimensions/config/InvigoratedDimensionsConfig.java
+++ src/main/java/com/bladecoldsteel/invigorateddimensions/config/InvigoratedDimensionsConfig.java
@@ -7,6 +7,7 @@ public final class InvigoratedDimensionsConfig {
     public static final ForgeConfigSpec SPEC;
 
     public static final ForgeConfigSpec.ConfigValue<Boolean> SPAWN_REPLACEMENT;
+    public static final ForgeConfigSpec.ConfigValue<String> DIFFICULTY_MODE;
 
     static {
         BUILDER.push("Invigorated Dimensions Config");
@@ -14,6 +15,9 @@ public final class InvigoratedDimensionsConfig {
         SPAWN_REPLACEMENT = BUILDER
                 .comment("Set this value to determine whether mobs, other than pixelmon, still spawn. | DEFAULT: true")
                 .define("spawn-replacement", true);
+        DIFFICULTY_MODE = BUILDER
+                .comment("Sets difficulty mode. | progressive: Boss stats scale based on how many bosses have been defeated. Starts similar to Ender Dragon and Wither | linear: Boss stats stay consistent throughout playthrough, similar to other Minecraft Bosses | DEFAULT: progressive")
+                .define("difficulty-mode", "progressive");
 
         BUILDER.pop();
         SPEC = BUILDER.build();
